1fda23e952f60398432473694dd55caf5153f468
Anuj Maurya <anuj.maurya@huawei.com>
2019-04-30 17:05:41 +0530

    update

--- cloud/edgecontroller/pkg/http_router/server.go
+++ cloud/edgecontroller/pkg/http_router/server.go
@@ -3,23 +3,20 @@ package http_router
 import (
 	"fmt"
 	"net/http"
-	// "encoding/base64"
+	"io/ioutil"
 
 	"github.com/kubeedge/beehive/pkg/core/model"
 	"github.com/kubeedge/beehive/pkg/common/log"
 	"github.com/kubeedge/beehive/pkg/core/context"
 	"github.com/kubeedge/kubeedge/cloud/edgecontroller/pkg/controller/messagelayer"
 	"github.com/kubeedge/kubeedge/edge/pkg/servicebus/util"
-	"io/ioutil"
 )
 
-type person struct {
-	Data string `json:"data"`
-	content  string `json:"Content-Type"`
-	Name string `json:"name"`
-}
-
 const nodeID  = "fb4ebb70-2783-42b8-b3ef-63e2fd6d242e"
+const demoPort = 8000
+const serverPort  = 8001
+const messageSource  = "router_rest"
+const routeGroup = "user"
 var cml messagelayer.ContextMessageLayer
 var httpRequest util.HTTPRequest
 
@@ -40,7 +37,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		log.LOGGER.Infof("data=%+v \n\n\n", data)
+		log.LOGGER.Infof("data=%+v \n", data)
 		msg := buildMessage(data, header)
 		if err := cml.Send(*msg); err != nil {
 			log.LOGGER.Warnf("send message failed with error: %s, operation: %s, resource: %s", err, msg.GetOperation(), msg.GetResource())
@@ -56,20 +53,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main(c *context.Context) {
 	cml = messagelayer.ContextMessageLayer{SendModuleName: "cloudhub", ReceiveModuleName: "", ResponseModuleName: "", Context: c}
 	http.HandleFunc("/", handler)
-	log.LOGGER.Infof("SERVER STARTED\n\n\n\n\n\n\n\n\n\n\n")
-	err := http.ListenAndServe(":8001", nil)
+	log.LOGGER.Infof("SERVER STARTED\n")
+	port := fmt.Sprintf(":%d", serverPort)
+	err := http.ListenAndServe(port, nil)
 	if err!=nil{
 		log.LOGGER.Fatalf("error listen ans serve :%e", err)
 	}
-
 }
 
 func buildMessage(data []byte, header http.Header) *model.Message{
 	msg := model.NewMessage("")
-	path := fmt.Sprintf("%s%s","8000:", header.Get("name"))
+	path := fmt.Sprintf("%d:%s",demoPort, header.Get("name"))
 	log.LOGGER.Infof("path:%s", path)
 	resource := fmt.Sprintf("%s%s%s%s%s", "node", "/", nodeID, "/", path)
-	msg.BuildRouter("router_rest", "user", resource, "POST")
+	msg.BuildRouter(messageSource, routeGroup, resource, "POST")
 	var err error
 	httpRequest.Body = data
 	if err != nil {
