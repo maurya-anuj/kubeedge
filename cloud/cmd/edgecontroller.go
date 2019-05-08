2ae78420f1ee3f5f18bd9da32d593efdc59741af
Anuj Maurya <anuj.maurya@huawei.com>
2019-05-08 16:56:23 +0530

    edit conf

--- cloud/cmd/edgecontroller.go
+++ cloud/cmd/edgecontroller.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/kubeedge/kubeedge/cloud/pkg/cloudhub"
 	_ "github.com/kubeedge/kubeedge/cloud/pkg/controller"
 	_ "github.com/kubeedge/kubeedge/cloud/pkg/devicecontroller"
+        _ "github.com/kubeedge/kubeedge/cloud/pkg/http_router"
 
 	"github.com/kubeedge/beehive/pkg/core"
 )
