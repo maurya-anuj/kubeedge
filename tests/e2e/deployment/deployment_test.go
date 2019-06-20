7a8e7073bc4db44a2b7abf5e5d2834063a95e431
Anuj Maurya <anuj.maurya@huawei.com>
2019-06-21 02:58:16 +0530

    delay b/w pod restart

--- tests/e2e/deployment/deployment_test.go
+++ tests/e2e/deployment/deployment_test.go
@@ -415,6 +415,7 @@ var _ = Describe("Application deployment test in E2E scenario", func() {
 				panic(err)
 			}
 			podlist = metav1.PodList{}
+			time.Sleep(time.Second * 1)
 			//deployment will restart it check again pod is there
 			podlist, err = utils.GetPods(ctx.Cfg.K8SMasterForKubeEdge+AppHandler+utils.LabelSelector+"app"+"%3D"+"client", "")
 			Expect(err).To(BeNil())
@@ -498,6 +499,7 @@ var _ = Describe("Application deployment test in E2E scenario", func() {
 				}
 			}
 			podlist = metav1.PodList{}
+			time.Sleep(time.Second * 1)
 			//deployment will restart it check again pod is there
 			podlist, err = utils.GetPods(ctx.Cfg.K8SMasterForKubeEdge+AppHandler+utils.LabelSelector+"app"+"%3D"+"server", "")
 			Expect(err).To(BeNil())
